753aab8bfcc87c546cdd5ded6e8b312418abc2f1
Tim Tanida <37686657+ttanida@users.noreply.github.com>
2021-01-11 19:18:35 +0100

    Create python file for tutorial

---
+++ pyqt_tutorial.py
@@ -0,0 +1,9 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+def window():
+	app = QApplication(sys.argv)
+	win = QMainWindow()
+	win.setGeometry(200, 200, 300, 300)
+	win.setWindowTitle("Tech with Tim")
